b514a351d3247e56b33224c001b7c478e533f700
AlejandroOrozco456 <alejandrogobass@gmail.com>
2022-02-18 09:16:45 -0600

    Se subieron los archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'introAngular';
+  title = 'Mi primera pagina';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { contenidoComponent } from './components/contenido/contenido.componet';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    contenidoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
